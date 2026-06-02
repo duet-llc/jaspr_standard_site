3e65fafe8d9b8f47b1dd1f33b783b8ce2000853c
Dhruvin Gandhi <contact@dhru.vin>
2026-06-02 17:00:32 +0530

    Configure Dart MCP server at project scope

---
+++ lib/jaspr_standard_site.dart
@@ -0,0 +1,5 @@
+/// Utilities for adding [standard.site](https://standard.site) support to
+/// [jaspr.site](https://docs.jaspr.site) websites.
+library;
+
+export 'src/jaspr_standard_site_base.dart';
